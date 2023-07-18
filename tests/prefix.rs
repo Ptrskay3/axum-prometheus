e41598cd9b1c2dcacd365151b85c9aeb26975e74
peter leeh <leeh123peter@gmail.com>
2023-07-18 10:06:30 +0200

    snapshot testing basics

---
+++ tests/prefix.rs
@@ -0,0 +1,34 @@
+mod common;
+use common::echo;
+
+use http::Request;
+use hyper::Body;
+use tower::{ServiceBuilder, ServiceExt};
+use tower_service::Service;
+
+#[tokio::test]
+async fn metric_handle_rendered_correctly_with_prefix() {
+    let (layer, handle) = axum_prometheus::PrometheusMetricLayerBuilder::new()
+        .with_prefix("pref")
+        .with_default_metrics()
+        .build_pair();
+
+    let mut service = ServiceBuilder::new().layer(layer).service_fn(echo);
+
+    let req = Request::builder().body(Body::empty()).unwrap();
+    let _res = service.ready().await.unwrap().call(req).await.unwrap();
+    insta::with_settings!({
+            filters =>
+            vec![
+                (
+                    r"\b[-+]?[0-9]*\.?[0-9]+\b\\npref_http_requests_duration_seconds_count",
+                    "",
+                )
+            ]
+        },
+    {
+
+        insta::assert_yaml_snapshot!(handle.render());
+    }
+    );
+}
